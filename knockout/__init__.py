57bd66598233efbe30723c67193ecda17c689c27
Jure Vrscaj <jure.vrscaj@gmail.com>
2010-10-23 08:23:39 +0200

    cleaned __init__.py

--- knockout/__init__.py
+++
@@ -1,6 +0,0 @@
-from gaeimport import FileCacheImporter as MyImporter
-
-def register(cache_dir="knocked", symlinks_dir=None):
-    MyImporter.CACHE_DIR = cache_dir
-    MyImporter.SYMLINKS_DIR = symlinks_dir
-    MyImporter.register()
