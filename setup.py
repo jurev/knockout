e3d5d22e58e15a94651155b8f1a40213d9274fd4
Jure Vrscaj <jure.vrscaj@gmail.com>
2018-02-25 17:12:35 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='knockout',
+      version='0.1',
+      description='Knockout aims to extend the basic importing mechanisms in Python by allowing more exotic stuff to be added to sys.path.',
+      url='http://github.com/jurev/knockout',
+      author='Jure Vrscaj',
+      author_email='jure.vrscaj@gmail.com',
+      license='MIT',
+      packages=['knockout'])
